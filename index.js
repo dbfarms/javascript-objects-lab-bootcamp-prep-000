901a3d0cfd685353109794bc36e10810e9ac4fb3
Matthew Hunger <matt.hunger@gmail.com>
2017-09-03 09:58:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {prop1: "1"}
 
-function updateObjectWithKeyAndValue(recipes, prop2, value) {
+function updateObjectWithKeyAndValue(recipes, prop2, 2) {
   return Object.assign({}, recipes, { prop2: ['2'] } )
 }
